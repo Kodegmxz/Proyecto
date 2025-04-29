14e9e889d63876cc1ccc7373cb0cff0e2c478a91
Jafetivy <josejafethgonzalezlopez@gmail.com>
2025-04-28 22:10:04 -0600

    cambios cambios cambios

---
+++ Ui/Login/a_login.py
@@ -0,0 +1,40 @@
+import sys
+import os
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QDial, QDialog, QApplication
+from PyQt5.uic import loadUi
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, parent_dir)
+
+class Login(QDialog):
+    def __init__(self, widget,db):
+        super(Login, self).__init__()
+        self.original_widget = widget  # Guardar referencia al widget
+        self.db = db  # Guardar referencia a la db
+        dir_l = os.path.dirname(os.path.abspath(__file__))
+        ui_l = os.path.join(dir_l, "u_login.ui")
+        loadUi(ui_l, self)
+        self.b_login.clicked.connect(self.loginbutton)
+
+
+    def loginbutton(self):
+        from Classes.users import usuario
+        usr = usuario(self.id_emp.text(), self.pwd_emp.text())
+        if self.id_emp.text() and self.pwd_emp.text():
+            usr.login(self.db, self.original_widget) # Referencia db y widged
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            pass
+
+
+def a1(db):
+    app = QApplication(sys.argv)
+    widget = QtWidgets.QStackedWidget()
+    mainwindow = Login(widget,db)  # Pasar el widget y db al inicio para poder ejecutar la db en otras funciones
+    widget.addWidget(mainwindow)
+    widget.setFixedWidth(1091)
+    widget.setFixedHeight(501)
+    widget.show()
+    sys.exit(app.exec_())
